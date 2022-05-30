47e1b0084628ccbde70c98a76eb5f16201b83365
grxwz4 <gerardoxwz4@gmail.com>
2022-05-30 08:31:10 -0500

    Initial commit

---
+++ app/src/main/java/com/example/reyclerview/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.reyclerview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.reyclerview.databinding.ActivityMainBinding
+import org.json.JSONObject
+import java.lang.reflect.Array
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val fakeVideoData: kotlin.Array<JSONObject> = arrayOf(
+            JSONObject("{\"title\": \"An awesome video title\", \"channel\": \"ChannelName\", \"views\": \"6k views\", \"datePosted\": \"5 days ago\", \"duration\": \"05:00\"}"),
+            JSONObject("{\"title\": \"John\", \"channel\": \"Channel\", \"views\": \"8k views\", \"datePosted\": \"5 days ago\", \"duration\": \"05:00\"}"),
+            JSONObject("{\"title\": \"John\", \"channel\": \"Channel\", \"views\": \"10k views\", \"datePosted\": \"5 days ago\", \"duration\": \"05:25\"}"),
+            JSONObject("{\"title\": \"John\", \"channel\": \"Channel\", \"views\": \"150k views\", \"datePosted\": \"A month ago\", \"duration\": \"05:02\"}"),
+            JSONObject("{\"title\": \"John\", \"channel\": \"Channel\", \"views\": \"254k views\", \"datePosted\": \"2 years ago\", \"duration\": \"05:00\"}")
+        )
+
+        binding.rvVideoEntries.adapter = MainAdapter(fakeVideoData)
+    }
+}
